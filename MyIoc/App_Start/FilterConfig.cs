87b4bbaf4f14f74364971f0554d3ea36236030fb
ejchen <785019257@qq.com>
2022-08-07 10:36:55 +0800

    全局注册过滤器

--- MyIoc/App_Start/FilterConfig.cs
+++ MyIoc/App_Start/FilterConfig.cs
@@ -1,5 +1,6 @@
 ﻿using System.Web;
 using System.Web.Mvc;
+using MyIoc.MyMvcFilters;
 
 namespace MyIoc
 {
@@ -8,6 +9,9 @@ namespace MyIoc
         public static void RegisterGlobalFilters(GlobalFilterCollection filters)
         {
             filters.Add(new HandleErrorAttribute());
+
+            //全局注册过滤器
+            filters.Add(new MyIndexActionFilter());
         }
     }
 }
